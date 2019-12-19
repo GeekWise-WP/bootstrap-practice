cb405f9b5970af82b91796cba80fa5e15bbe7fc6
Robert Nelson <RobNelson2989@gmail.com>
2019-12-19 13:02:10 -0800

    added some bootstrap

--- wordpress/wp-content/themes/bootstrap-practice/index.php
+++ wordpress/wp-content/themes/bootstrap-practice/index.php
@@ -1,5 +1,35 @@
 <?php get_header(); ?>
 
-<h1>Monkey D. Luffy, Captain of the Straw Hats.</h1>
+<div class= "container">
+    <div class="d-flex row justify-content-lg-center">
+        <div class="col-lg">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">One Piece</a>
+        </div>
+    </div>
+</div>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">The Captain <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">The Crew</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">The Goal</a>
+      </li>
+    </ul>
+  </div>
 
+</nav>
+
+<main>
+<div class="container">
+  <div class="row">
+    <div class="col-lg"> Straw Hat Crew</div>
+    </div>
+    </div>
+</main>
+</div>
 <?php get_footer(); ?>
